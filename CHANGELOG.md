29aea699ef75eff4a71707d481c131014a39384f
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-05-15 15:38:16 +0300

    Prepare the next release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 <!-- changelogging: start -->
 
+## 1.0.1 (2023-05-15)
+
+### Fixes
+
+- Added missing `typing-extensions` dependency.
+
 ## 1.0.0 (2023-04-16)
 
 Initial release.
--- mixed_methods/__init__.py
+++ mixed_methods/__init__.py
@@ -6,7 +6,7 @@ __url__ = "https://github.com/nekitdev/mixed-methods"
 __title__ = "mixed_methods"
 __author__ = "nekitdev"
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from mixed_methods.core import MixedMethod, mixed_method
 
